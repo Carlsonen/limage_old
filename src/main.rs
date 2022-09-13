fc30327268208a397af6fa4ea2b5974565343cbc
aftix <aftix@aftix.xyz>
2022-09-13 09:25:48 -0500

    Simple grid pattern

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use limage::Limage;
+use limage::{patterns::Grid, Limage};
 
 fn main() {
-    let img = Limage::new(360, 360);
+    let img = Limage::new(360, 360)
+        .with_color([255; 3])
+        .with_pattern(Grid::new(3, 20, 20));
 
     img.save("shit.png").unwrap();
 }
---
+++ src/patterns.rs
@@ -0,0 +1,58 @@
+pub trait Pattern {
+    fn draw(self, img: &mut super::Limage);
+}
+
+// TODO: Different alignments on grid
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+pub struct Grid {
+    width: u32,
+    x_skip: u32,
+    y_skip: u32,
+    color: [u8; 3],
+}
+
+impl Grid {
+    pub fn new(width: u32, x_skip: u32, y_skip: u32) -> Self {
+        Self {
+            width,
+            x_skip,
+            y_skip,
+            color: [0; 3],
+        }
+    }
+
+    pub fn with_color(mut self, color: [u8; 3]) -> Self {
+        self.color = color;
+        self
+    }
+}
+
+impl Pattern for Grid {
+    fn draw(self, img: &mut super::Limage) {
+        let (width, height) = (img.width(), img.height());
+
+        let mut x = self.x_skip + self.width / 2;
+        while x < width {
+            for offset in -(self.width as i64) / 2..=(self.width as i64) / 2 {
+                img.draw_line(
+                    (x as i64 + offset, 0).into(),
+                    (x as i64 + offset, height as i64 - 1).into(),
+                    self.color,
+                );
+            }
+            x += self.x_skip;
+        }
+
+        let mut y = self.y_skip + self.width / 2;
+        while y < height {
+            for offset in -(self.width as i64) / 2..=(self.width as i64) / 2 {
+                img.draw_line(
+                    (0, y as i64 + offset).into(),
+                    (width as i64 - 1, y as i64 + offset).into(),
+                    self.color,
+                );
+            }
+            y += self.y_skip;
+        }
+    }
+}
