5c4f7975f78c1a49eaf1e0eae07076511a395760
Carlsonen <ocarlsson2002@gmail.com>
2022-09-15 15:07:38 +0200

    ok what

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use limage::Limage;
+use limage::*;
 
 fn main() {
-    let img = Limage::new(360, 360);
+    let mut img = Limage::new(100, 100);
+
+    for (x, y) in circle(50, 50, 20) {
+        if x >= 0 && y >= 0 {
+            img.put_hsl(
+                x as u32,
+                y as u32,
+                [
+                    (x + y) as f32 * 360f32 / 40.0,
+                    1.0,
+                    1.0 - y as f32 / img.height() as f32,
+                ],
+            );
+        }
+    }
 
     img.save("shit.png").unwrap();
 }
--- src/shape_iterators.rs
+++ src/shape_iterators.rs
@@ -1,6 +1,3 @@
-
-
-
 pub struct Rectangle {
     pos_x: i32,
     width: i32,
@@ -8,35 +5,73 @@ pub struct Rectangle {
     step_x: i32,
     step_y: i32,
     x: i32,
-    y: i32
+    y: i32,
 }
 impl Rectangle {
     pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
         let step_x = match w {
             _ if w < 0 => -1,
-            _ => 1
+            _ => 1,
         };
         let step_y = match h {
             _ if h < 0 => -1,
-            _ => 1
+            _ => 1,
         };
-    
-        Rectangle { pos_x: x, width: w, height: h, step_x: step_x, step_y: step_y, x: x, y: y }
+
+        Rectangle {
+            pos_x: x,
+            width: w,
+            height: h,
+            step_x: step_x,
+            step_y: step_y,
+            x: x,
+            y: y,
+        }
     }
 }
 impl Iterator for Rectangle {
     type Item = (i32, i32);
 
-    fn next(&mut self) -> Option<Self::Item> {    
-        self.x += self.step_x;
+    fn next(&mut self) -> Option<Self::Item> {
         if self.x == self.width {
             self.y += self.step_y;
             if self.y == self.height {
-                return None
+                return None;
             }
             self.x = self.pos_x;
         }
-        
-        Some((self.x, self.y))
+        self.x += self.step_x;
+        Some((self.x - 1, self.y))
+    }
+}
+
+pub struct Circle {
+    bounds: Rectangle,
+    origin: (i32, i32),
+    radius: i32,
+}
+impl Circle {
+    pub fn new(x: i32, y: i32, radius: i32) -> Self {
+        Circle {
+            bounds: Rectangle::new(x - radius, y - radius, x + radius + 1, y + radius + 1),
+            origin: (x, y),
+            radius,
+        }
+    }
+}
+impl Iterator for Circle {
+    type Item = (i32, i32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let coord = match self.bounds.next() {
+                Some(coord) => coord,
+                None => return None,
+            };
+            let c = (coord.0 - self.origin.0, coord.1 - self.origin.1);
+            if c.0 * c.0 + c.1 * c.1 <= self.radius * (1 + self.radius) {
+                return Some(coord);
+            }
+        }
     }
 }
